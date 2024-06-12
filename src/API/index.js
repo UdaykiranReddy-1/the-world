828d7674af4d21c0d51acfe15118b0695fcb8bfb
UdaykiranReddy-1 <118445671+UdaykiranReddy-1@users.noreply.github.com>
2024-06-12 23:47:44 +0530

    Update "BaseURL" of api

--- src/API/index.js
+++ src/API/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://restcountries.com/v2/";
+const BASE_URL = "https://restcountries.com/v2";
 
 export function getAllCountries(){
     return axios.get(`${BASE_URL}/all`);
