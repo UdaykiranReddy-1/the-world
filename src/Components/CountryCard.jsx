4e334c7780bc7304748d4d565a58298be6bf6757
UdaykiranReddy-1 <118445671+UdaykiranReddy-1@users.noreply.github.com>
2024-07-11 02:01:46 +0530

    card ui update

--- src/Components/CountryCard.jsx
+++ src/Components/CountryCard.jsx
@@ -1,40 +1,24 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {FaBuildingFlag} from "react-icons/fa6"
-import {BsPeopleFill} from "react-icons/bs"
+import { FaBuildingFlag } from "react-icons/fa6"
+import { BsPeopleFill } from "react-icons/bs"
 // import {GrCurrency} from "react-icons/gr"
 
-export default function CountryCard({name,flagUrl,capital, population}) {
+export default function CountryCard({ name, flagUrl, capital, population }) {
 
   return (
-    <Card sx={{ maxWidth: 345 , width:220 , height:300}}>
-      <CardMedia
-        component="img"
-        sx={{ height: 140 }}
-        image={flagUrl}
-      />
-      <CardContent>
-          <h3 className="title long-name">
-          {name}
-          </h3>
-        
-        <Typography variant="body2" color="text.secondary" component="span">
-          <div className="details">
-            <div className="capital">
-            <FaBuildingFlag className='icon'/>{capital} 
-            </div>
-            {/* <div className="currency">
-             <GrCurrency className='icon'/>{currency?.map((curr)=> curr.name).join(',')}
-            </div> */}
-            <div className="population">
-            <BsPeopleFill className="icon"/>{population}
-            </div>
+    <div class="card">
+      <img class="card-media" src={flagUrl} alt="Country Flag" />
+      <div class="card-content">
+        <h3 class="title long-name">{name}</h3>
+        <div class="details">
+          <div class="capital">
+            <FaBuildingFlag className='icon' /> &nbsp; {capital ? capital : "N/A"}
           </div>
-        </Typography>
-      </CardContent>
-    </Card>
+          <div class="population">
+            <BsPeopleFill className="icon" /> &nbsp; {population ? population : "N/A"}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
